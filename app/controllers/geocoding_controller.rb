14e8ae4814f06479f4c39aa32731c922c130cca4
Kate Kelly <kmkelly1@chicagobooth.edu>
2017-07-10 01:23:08 +0000

    Latitude and longitude
    gets the latitude and longitude, but still needs the street address formatted

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -14,12 +14,30 @@ class GeocodingController < ApplicationController
     #
     # The street address that the user typed is in the variable @street_address.
     # ==========================================================================
+    @url = "http://maps.googleapis.com/maps/api/geocode/json?address="
+    
+    @street_address_with_spaces = []
+    
+    @street_address = @street_address.split
+    
+    @street_address.each do |word|
+      new_word = word + "+"
+      @street_address_with_spaces.push(new_word)
+    end
+    
+    @street_address_with_spaces = @street_address_with_spaces.to_s
+    
+    @google_url = @url + @street_address_with_spaces
+    
+    parsed_data = JSON.parse(open(@google_url).read)
+      # latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+      # longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+    
+  
 
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
 
-
-    @latitude = "Replace this string with your answer."
-
-    @longitude = "Replace this string with your answer."
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
     render("geocoding/street_to_coords.html.erb")
   end
