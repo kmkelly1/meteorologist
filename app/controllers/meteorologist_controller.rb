fe9ad3aced5b88d9d090a91de7413ee7d3bd4935
Kate Kelly <kmkelly1@chicagobooth.edu>
2017-07-12 12:28:54 +0000

    added api key

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -28,7 +28,7 @@ class MeteorologistController < ApplicationController
     
     @street_address_with_spaces = @street_address_with_spaces.to_s
     
-    @google_url = @url + @street_address_with_spaces + "&"
+    @google_url = @url + @street_address_with_spaces + "&=AIzaSyDNqz-qn2W4rtt6pVaNwlw3nWYOgyzDHiw"
     
     parsed_data = JSON.parse(open(@google_url).read)
       # latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
